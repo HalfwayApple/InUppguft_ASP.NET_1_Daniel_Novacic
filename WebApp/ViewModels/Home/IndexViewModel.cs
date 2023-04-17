62bdb59e8bfb4e6c39013cbf8384a12221e5fc7e
xeslaLaptop <xesla@DANIELS-LAPTOP>
2023-04-17 09:26:31 +0200

    Add project files.

---
+++ WebApp/ViewModels/Home/IndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿using WebApp.Models;
+
+namespace WebApp.ViewModels.Home;
+
+public class IndexViewModel
+{
+	public CollectionModel? CollectionModel { get; set; }
+	public ShowcaseModel? ShowcaseModel { get; set; }
+	public SpecialOffersModel? SpecialOffersModel { get; set; }
+}
